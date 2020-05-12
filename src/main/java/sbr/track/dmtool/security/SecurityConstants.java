37fd7b04ed55ad2d58db099798d9846fbcc12327
Pr0sper0 <gestor009@gmail.com>
2020-05-12 17:58:03 +0300

    JWT token time expiration extended to 60 minutes

--- src/main/java/sbr/track/dmtool/security/SecurityConstants.java
+++ src/main/java/sbr/track/dmtool/security/SecurityConstants.java
@@ -7,6 +7,6 @@ public class SecurityConstants {
     public static final String SECRET = "SecretKeyToGenJWTs";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final Long EXPIRATION_TIME = Long.valueOf(900_000);
+    public static final Long EXPIRATION_TIME = Long.valueOf(3600_000); //50 minutes
 
 }
