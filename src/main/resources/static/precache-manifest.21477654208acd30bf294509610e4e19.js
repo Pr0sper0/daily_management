488570a26b3ee3bf086d509cf45001c044729601
Pr0sper0 <gestor009@gmail.com>
2020-05-12 18:12:40 +0300

    react part added

---
+++ src/main/resources/static/precache-manifest.21477654208acd30bf294509610e4e19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42cf90332dbe7d628877",
+    "url": "/static/css/main.b5eab8dd.chunk.css"
+  },
+  {
+    "revision": "42cf90332dbe7d628877",
+    "url": "/static/js/main.85e19fde.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8dfcd59eaa90fc15b329",
+    "url": "/static/css/2.9c780625.chunk.css"
+  },
+  {
+    "revision": "8dfcd59eaa90fc15b329",
+    "url": "/static/js/2.f709cffe.chunk.js"
+  },
+  {
+    "revision": "4ebb4321c69ceed64b02c5904d03ea22",
+    "url": "/index.html"
+  }
+];
